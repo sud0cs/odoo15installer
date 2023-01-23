100a25faad7c37dd71cdfa17a41dca9da9aba7f3
sud0cs <87086245+sud0cs@users.noreply.github.com>
2023-01-23 20:35:17 +0100

    Update odooinstall.sh

--- odooinstall.sh
+++ odooinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo clear
-if [ "$UID" -eq 0 ] 
+username=$(sudo echo "i")
+if [ $username = "i" ] 
 then
     echo 
     echo "########################################"
@@ -13,14 +13,14 @@ then
     read -p "Are you sure? " -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        clear
+        sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf > /dev/null
         echo Updating...
         echo ""
         sudo apt-get update > /dev/null
         echo "#######################################"
         echo ""
         echo Installing Python and Postgres...
-        sudo apt-get install python3 python3-pip postgresql postgresql-client git -yq
+        sudo apt-get install python3 python3-pip postgresql postgresql-client git -yq > /dev/null
         echo ""
         echo "#######################################"
         echo ""
@@ -33,15 +33,15 @@ then
         focal-security main"
         clear
         echo Installing dependencies...
-        sudo apt-get -yq update
-        sudo apt-get install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev libssl1.1 zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev software-properties-common libxrender1 libfontconfig1 libx11-dev libjpeg62 libxtst6 fontconfig xfonts-75dpi xfonts-base libjpeg-turbo8 wget -yq
+        sudo apt-get -yq update > /dev/null
+        sudo apt-get install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev libssl1.1 zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev software-properties-common libxrender1 libfontconfig1 libx11-dev libjpeg62 libxtst6 fontconfig xfonts-75dpi xfonts-base libjpeg-turbo8 wget -yq > /dev/null
         echo ""
         echo "#######################################"
         echo ""
         echo Installing required Python packages...
         pip3 install setuptools wheel PyPDF2 > /dev/null
         pip3 install -r "/home/$(whoami)/odoo/requirements.txt" > /dev/null
-        wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb -O pdf.deb
+        wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb -O "/home/$(whoami)/pdf.deb" > /dev/null
         sudo dpkg -i "/home/$(whoami)/pdf.deb" > /dev/null
         "/home/$(whoami)/odoo/odoo-bin"
     fi  
