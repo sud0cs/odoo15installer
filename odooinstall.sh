9a176d27fe67331a36d920f3c6109c243388d839
sud0cs <87086245+sud0cs@users.noreply.github.com>
2023-01-24 17:45:28 +0100

    Update odooinstall.sh

--- odooinstall.sh
+++ odooinstall.sh
@@ -38,7 +38,8 @@ then
         sudo apt-get install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev libssl1.1 zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev software-properties-common libxrender1 libfontconfig1 libx11-dev libjpeg62 libxtst6 fontconfig xfonts-75dpi xfonts-base libjpeg-turbo8 wget -yq > /dev/null
         clear
         echo Installing required Python packages...
-        pip3 install setuptools wheel PyPDF2 Babel chardet cryptography decorator docutils ebaysdk freezegun gevent greenlet idna Jinja2 libsass lxml MarkupSafe num2words ofxparse Pillow polib psutil psycopg2 pydot pyopenssl pyserial python-dateutil python-stdnum pytz pyusb qrcode reportlab requests urllib3 vobject Werkzeug xlrd XlsxWriter xlwt zeep
+        pip3 install setuptools
+        pip3 install -r "/home/$(whoami)/odoo/requirements.txt"
         wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb -O "/home/$(whoami)/pdf.deb"
         sudo dpkg -i "/home/$(whoami)/pdf.deb"
         "/home/$(whoami)/odoo/odoo-bin"
