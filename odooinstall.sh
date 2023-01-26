ec81d2c1cccafa73882dc97a2e98391987671e78
sud0cs <87086245+sud0cs@users.noreply.github.com>
2023-01-26 16:57:07 +0100

    Update odooinstall.sh

--- odooinstall.sh
+++ odooinstall.sh
@@ -11,7 +11,7 @@ then
     echo "########################################"
     echo ""
     echo ""
-    read -p "Are you sure[y/N]? " -n 1 -r
+    read -p "Do you want to proceed installing Odoo on the user $(whoami)[y/N]? " -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         clear
@@ -38,13 +38,13 @@ then
         sudo apt-get install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev libssl1.1 zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev software-properties-common libxrender1 libfontconfig1 libx11-dev libjpeg62 libxtst6 fontconfig xfonts-75dpi xfonts-base libjpeg-turbo8 wget -yq > /dev/null
         clear
         echo Installing required Python packages...
-        pip3 install setuptools wheel
-        python3 -m venv odoovenv
-        source odoovenv/bin/activate
-        pip3 install setuptools wheel
-        pip3 install -r "/home/$(whoami)/odoo/requirements.txt"
-        wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb -O "/home/$(whoami)/pdf.deb"
-        sudo dpkg -i "/home/$(whoami)/pdf.deb"
+        pip3 install setuptools wheel > /dev/null
+        python3 -m venv odoovenv > /dev/null
+        source odoovenv/bin/activate > /dev/null
+        pip3 install setuptools wheel > /dev/null
+        pip3 install -r "/home/$(whoami)/odoo/requirements.txt" > /dev/null
+        wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb -O "/home/$(whoami)/pdf.deb" > /dev/null
+        sudo dpkg -i "/home/$(whoami)/pdf.deb" > /dev/null
         "/home/$(whoami)/odoo/odoo-bin"
     fi  
 fi
